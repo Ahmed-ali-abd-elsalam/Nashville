f1a0b98d5eaaf676e9c1b6bcafdcbcbffa2b1314
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2024-11-29 19:30:47 +0200

    all that is left is more eda and visualizations

--- Analysis queries.sql
+++ Analysis queries.sql
@@ -1,53 +1,96 @@
-SELECT
-	*
-FROM
-	NASHFULL
-LIMIT
-	10;
-
--- number of units sold in each price range  
-SELECT
-	"Sold_As_Vacant",
-	CEIL("Sale_Price" / 200000+1) * 200000 AS "Price_Range",
-	COUNT(CEIL("Sale_Price" / 200000+1)) AS "No. OF Units"
-FROM
-	NASHFULL
-GROUP BY
-	"Sold_As_Vacant",
-	CEIL("Sale_Price" / 200000+1)
-ORDER BY
-	CEIL("Sale_Price" / 200000+1 );
-
--- NUMBER OF UNITS SOLD WITH MULTIPLE PARCELS 
-SELECT "Multiple_Parcels_Involved_in_Sale","Sold_As_Vacant" , COUNT(*)
+SELECT *
 FROM NASHFULL
-GROUP BY "Multiple_Parcels_Involved_in_Sale","Sold_As_Vacant"
-ORDER BY "Multiple_Parcels_Involved_in_Sale","Sold_As_Vacant";
-
--- NUMBER OF UNITS SOLD AS VACANT DIVIDED BY ACREAGE
-SELECT
+LIMIT 10;
+-- number of units sold in each price range  
+SELECT CEIL("Sale_Price" / 200000 + 1) * 200000 AS "Price_Range",
+	COUNT(CEIL("Sale_Price" / 200000 + 1)) AS "No. OF Units"
+FROM NASHFULL
+GROUP BY "Sold_As_Vacant",
+	CEIL("Sale_Price" / 200000 + 1)
+ORDER BY CEIL("Sale_Price" / 200000 + 1);
+-- number of units vacant or full
+SELECT "Sold_As_Vacant",
+	COUNT("Sold_As_Vacant")
+FROM NASHFULL
+GROUP BY "Sold_As_Vacant";
+-- NUMBER OF UNITS SOLD WITH MULTIPLE PARCELS 
+SELECT "Multiple_Parcels_Involved_in_Sale",
 	"Sold_As_Vacant",
-	CEIL("Acreage" / .1) * 0.1 AS "Acreage",
-	COUNT(CEIL("Acreage" / .1)) AS "No. OF Units"
-FROM
-	NASHFULL
+	COUNT(*)
+FROM NASHFULL
+GROUP BY "Multiple_Parcels_Involved_in_Sale",
+	"Sold_As_Vacant"
+ORDER BY "Multiple_Parcels_Involved_in_Sale",
+	"Sold_As_Vacant";
+-- NUMBER OF UNITS SOLD AS VACANT DIVIDED BY ACREAGE
+SELECT "Sold_As_Vacant",
+	CEIL("Acreage" /.1) * 0.1 AS "Acreage",
+	COUNT(CEIL("Acreage" /.1)) AS "No. OF Units"
+FROM NASHFULL
 WHERE "Sold_As_Vacant"
-GROUP BY
-	"Sold_As_Vacant",
+GROUP BY "Sold_As_Vacant",
 	CEIL("Acreage" / 0.1)
-ORDER BY
-	CEIL("Acreage" / 0.1);
-
--- NUMBER OF UNITS SOLD AS VACANT DIVIDED BY ACREAGE
-SELECT
-	"Sold_As_Vacant",
+ORDER BY CEIL("Acreage" / 0.1);
+-- NUMBER OF UNITS SOLD AS VACANT DIVIDED BY Grade
+SELECT "Sold_As_Vacant",
 	"Grade",
 	COUNT("Grade") AS "No. OF Units"
-FROM
-	NASHFULL
+FROM NASHFULL
 WHERE "Sold_As_Vacant"
-GROUP BY
+GROUP BY "Sold_As_Vacant",
+	"Grade"
+ORDER BY "Grade";
+-- NUMBER OF UNITS SOLD avg price PER MONTH
+SELECT EXTRACT(
+		YEAR
+		FROM "Sale_Date"
+	) AS "YEAR",
+	EXTRACT(
+		MONTH
+		FROM "Sale_Date"
+	) AS "MONTH",
+	count("Sale_Date"),
+	ROUND(AVG("Sale_Price"), 2) "PRICE"
+FROM NASHFULL
+GROUP BY EXTRACT(
+		YEAR
+		FROM "Sale_Date"
+	),
+	EXTRACT(
+		MONTH
+		FROM "Sale_Date"
+	)
+ORDER BY EXTRACT(
+		YEAR
+		FROM "Sale_Date"
+	),
+	EXTRACT(
+		MONTH
+		FROM "Sale_Date"
+	);
+-- NUMBER OF UNITS SOLD avg price PER MONTH
+SELECT EXTRACT(
+		YEAR
+		FROM "Sale_Date"
+	) AS "YEAR",
 	"Sold_As_Vacant",
-	"Grade"
-ORDER BY
-	"Grade"
+	count("Sale_Date") AS "No. Of Units",
+	ROUND(AVG("Sale_Price"), 2) "PRICE"
+FROM NASHFULL
+GROUP BY EXTRACT(
+		YEAR
+		FROM "Sale_Date"
+	),
+	"Sold_As_Vacant"
+ORDER BY EXTRACT(
+		YEAR
+		FROM "Sale_Date"
+	),
+	"Sold_As_Vacant";
+-- average price per city
+SELECT "Property_City",
+	round(avg("Sale_Price"), 2) AS "Average_Price"
+FROM NASHFULL
+GROUP BY "Property_City"
+ORDER BY "Average_Price" desc,
+	"Property_City";
--- Cleaning queries.sql
+++ Cleaning queries.sql
@@ -1,13 +1,9 @@
 -- create a backup of the data in a seperate table
-SELECT
-	* INTO NASHVILLE_HOUSING
-FROM
-	NASHVILLE_HOUSING_PERM;
-
+SELECT * INTO NASHVILLE_HOUSING
+FROM NASHVILLE_HOUSING_PERM;
 -- remove whitespaces from the data
 UPDATE NASHVILLE_HOUSING
-SET
-	"Parcel_ID" = TRIM("Parcel_ID"),
+SET "Parcel_ID" = TRIM("Parcel_ID"),
 	"Land_Use" = TRIM("Land_Use"),
 	"Property_Address" = TRIM("Property_Address"),
 	"Property_City" = TRIM("Property_City"),
@@ -18,136 +14,97 @@ SET
 	"Foundation_Type" = TRIM("Foundation_Type"),
 	"Exterior_Wall" = TRIM("Exterior_Wall"),
 	"Grade" = TRIM("Grade");
-
 -- unifying the land_use to a single category
 UPDATE NASHVILLE_HOUSING AS NS
-SET
-	"Land_Use" = 'VACANT RESIDENTIAL LAND'
-WHERE
-	NS."Land_Use" = 'VACANT RES LAND';
-
+SET "Land_Use" = 'VACANT RESIDENTIAL LAND'
+WHERE NS."Land_Use" = 'VACANT RES LAND';
 -- filling the missing addresses in the data from the extra column Address then removing it later
 UPDATE NASHVILLE_HOUSING
-SET
-	"Property_Address" = "Address"
-WHERE
-	"Property_Address" IS NULL
+SET "Property_Address" = "Address"
+WHERE "Property_Address" IS NULL
 	AND "Address" IS NOT NULL;
-
 -- filling the missing cities in the data from the extra column City then removing it later
 UPDATE NASHVILLE_HOUSING
-SET
-	"Property_City" = "City"
-WHERE
-	"Property_City" IS NULL
+SET "Property_City" = "City"
+WHERE "Property_City" IS NULL
 	AND "City" IS NOT NULL;
-
 -- renaming the column to distinguish betweeen the rows with same parcel_id
 ALTER TABLE NASHVILLE_HOUSING
-RENAME COLUMN "Unnamed_0" TO "Unique";
-
+	RENAME COLUMN "Unnamed_0" TO "Unique";
 /*
-I discovered that some parcels appear multiple times, with address values missing in one occurrence but present in another.
-This query is designed to populate the missing address values for repeated parcel IDs.
-*/
+ I discovered that some parcels appear multiple times, with address values missing in one occurrence but present in another.
+ This query is designed to populate the missing address values for repeated parcel IDs.
+ */
 UPDATE NASHVILLE_HOUSING AS NS
-SET
-	"Property_Address" = FULL_TABLE."Property_Address"
-FROM
-	(
-		SELECT
-			A."Unique",
+SET "Property_Address" = FULL_TABLE."Property_Address"
+FROM (
+		SELECT A."Unique",
 			B."Property_Address"
-		FROM
-			NASHVILLE_HOUSING AS A
+		FROM NASHVILLE_HOUSING AS A
 			INNER JOIN NASHVILLE_HOUSING AS B ON A."Parcel_ID" = B."Parcel_ID"
 			AND A."Unique" <> B."Unique"
-		WHERE
-			A."Property_Address" IS NULL
+		WHERE A."Property_Address" IS NULL
 	) AS FULL_TABLE
-WHERE
-	NS."Unique" = FULL_TABLE."Unique";
-
+WHERE NS."Unique" = FULL_TABLE."Unique";
 -- fixing the data types for columns (Sale_Date,Year_Built,Sold_As_Vacant,Multiple_Parcels_Involved_in_Sale,Bedrooms,Full_Bath,Half_Bath)
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Sale_Date"
 SET DATA TYPE DATE USING "Sale_Date"::DATE;
-
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Year_Built"
 SET DATA TYPE INTEGER USING "Year_Built"::INTEGER;
-
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Sold_As_Vacant"
 SET DATA TYPE BOOLEAN USING "Sold_As_Vacant"::BOOLEAN;
-
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Multiple_Parcels_Involved_in_Sale"
 SET DATA TYPE BOOLEAN USING "Multiple_Parcels_Involved_in_Sale"::BOOLEAN;
-
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Bedrooms"
 SET DATA TYPE INTEGER USING "Bedrooms"::INTEGER;
-
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Full_Bath"
 SET DATA TYPE INTEGER USING "Full_Bath"::INTEGER;
-
 ALTER TABLE NASHVILLE_HOUSING
 ALTER COLUMN "Half_Bath"
 SET DATA TYPE INTEGER USING "Half_Bath"::INTEGER;
-
 -- dropping extra unneeded columns
-ALTER TABLE NASHVILLE_HOUSING
-DROP COLUMN "Unnamed_01",
-DROP COLUMN "Legal_Reference",
-DROP COLUMN "image",
-DROP COLUMN "Owner_Name",
-DROP COLUMN "Suite_Condo",
-DROP COLUMN "State",
-DROP COLUMN "Address",
-DROP COLUMN "City";
-
+ALTER TABLE NASHVILLE_HOUSING DROP COLUMN "Unnamed_01",
+	DROP COLUMN "Legal_Reference",
+	DROP COLUMN "image",
+	DROP COLUMN "Owner_Name",
+	DROP COLUMN "Suite_Condo",
+	DROP COLUMN "State",
+	DROP COLUMN "Address",
+	DROP COLUMN "City";
 /*  
-I observed that there are duplicate rows, which I defined as rows sharing the same parcel ID, property address, sale date, and sale price. 
-This query is intended to identify and remove those duplicate rows.
-*/
-WITH
-	DUPLICATED AS (
-		SELECT
-			"Unique",
-			"NO"
-		FROM
-			(
-				SELECT
-					ROW_NUMBER() OVER (
-						PARTITION BY
-							A."Parcel_ID"
-					) AS "NO",
-					A."Unique",
-					A."Parcel_ID",
-					A."Property_Address",
-					A."Sale_Date",
-					A."Sale_Price"
-				FROM
-					NASHVILLE_HOUSING AS A
-					INNER JOIN NASHVILLE_HOUSING AS B ON A."Parcel_ID" = B."Parcel_ID"
-					AND A."Property_Address" = B."Property_Address"
-					AND A."Sale_Date" = B."Sale_Date"
-					AND A."Sale_Price" = B."Sale_Price"
-					AND A."Unique" <> B."Unique"
-			)
-		WHERE
-			"NO" > 1
-		ORDER BY
-			"Unique",
-			"NO"
-	)
+ I observed that there are duplicate rows, which I defined as rows sharing the same parcel ID, property address, sale date, and sale price. 
+ This query is intended to identify and remove those duplicate rows.
+ */
+WITH DUPLICATED AS (
+	SELECT "Unique",
+		"NO"
+	FROM (
+			SELECT ROW_NUMBER() OVER (PARTITION BY A."Parcel_ID") AS "NO",
+				A."Unique",
+				A."Parcel_ID",
+				A."Property_Address",
+				A."Sale_Date",
+				A."Sale_Price"
+			FROM NASHVILLE_HOUSING AS A
+				INNER JOIN NASHVILLE_HOUSING AS B ON A."Parcel_ID" = B."Parcel_ID"
+				AND A."Property_Address" = B."Property_Address"
+				AND A."Sale_Date" = B."Sale_Date"
+				AND A."Sale_Price" = B."Sale_Price"
+				AND A."Unique" <> B."Unique"
+		)
+	WHERE "NO" > 1
+	ORDER BY "Unique",
+		"NO"
+)
 DELETE FROM NASHVILLE_HOUSING AS NS USING DUPLICATED
-WHERE
-	NS."Unique" = DUPLICATED."Unique"
-SELECT
-	SUM(
+WHERE NS."Unique" = DUPLICATED."Unique";
+SELECT SUM(
 		CASE
 			WHEN NSA."Unique" IS NOT NULL
 			AND NSA."Parcel_ID" IS NOT NULL
@@ -209,15 +166,10 @@ SELECT
 		) / SUM(1),
 		2
 	) * 100 AS "RATIO"
-FROM
-	NASHVILLE_HOUSING AS NSA;
-
-SELECT
-	* INTO NASHFULL
-FROM
-	NASHVILLE_HOUSING AS NSA
-WHERE
-	NSA."Unique" IS NOT NULL
+FROM NASHVILLE_HOUSING AS NSA;
+SELECT * INTO NASHFULL
+FROM NASHVILLE_HOUSING AS NSA
+WHERE NSA."Unique" IS NOT NULL
 	AND NSA."Parcel_ID" IS NOT NULL
 	AND NSA."Land_Use" IS NOT NULL
 	AND NSA."Property_Address" IS NOT NULL
